d8c2bf9fb1bfae40a261d412519963f61f2e3a7c
zitacheng <zita.cheng@epitech.eu>
2020-03-29 22:54:56 +0200

    home carousel

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Carousel from 'react-bootstrap/Carousel';
 
 class Home extends Component {
 
@@ -15,6 +17,31 @@ class Home extends Component {
               <form>
                 <input type="text" name="name" />
               </form>
+              <Col className="w-50 mx-auto mt-4 mb-4">
+                <Carousel className="w-25 mx-auto" interval={1000000} indicators={false}>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={require("../assets/dead.png")}
+                      alt="First slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={require("../assets/laugh.png")}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src={require("../assets/tired.png")}
+                      alt="Third slide"
+                    />
+                  </Carousel.Item>
+                </Carousel>
+              </Col>
             <Button className="mt-3" variant="outline-warning" onClick={() => {window.location = "/lobby/"}}>Create private room</Button>
           </Card.Body>
         </Card>
